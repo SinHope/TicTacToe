f74bdadc687a2a285273402e5bc8307b7065967d
SinHope <88398163+SinHope@users.noreply.github.com>
2022-07-07 23:26:05 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+// alert("Welcome to a small project of mine! A simple game of TicTacToe to reminisce our childhood. Enjoy!");
+
+$(".button1").click("text", "O")
+
+
+
+$(".button2").click(function myFunction2(){
+  $(".button2").css("background-color", "red");
+});
+$(".button3").click(function myFunction3(){
+  $(".button3").css("background-color", "red");
+});
+$(".button4").click(function myFunction4(){
+  $(".button4").css("background-color", "red");
+});
+$(".button5").click(function myFunction5(){
+  $(".button5").css("background-color", "red");
+});
+$(".button6").click(function myFunction6(){
+  $(".button6").css("background-color", "red");
+});
+$(".button7").click(function myFunction7(){
+  $(".button7").css("background-color", "red");
+});
+$(".button8").click(function myFunction8(){
+  $(".button8").css("background-color", "red");
+});
+$(".button9").click(function myFunction9(){
+  $(".button9").css("background-color", "red");
+});
